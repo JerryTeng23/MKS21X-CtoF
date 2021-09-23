a46455d2d2d8cf0e7103f8627157520b85f13910
Jerry Teng <jteng30@stuy.edu>
2021-09-23 18:06:36 -0400

    - Added comments regarding the parameters and return values.

---
+++ CtoFTester.java
@@ -0,0 +1,20 @@
+public class CtoFTester{
+
+  /* The first function should have the degrees, in Celsius, as the parameter, and
+     return the equivalent degrees in Fahrenheit. The latter function should switch
+     parameter and what it returns. To help with accuracy, it is helpful for the parameters
+     to be a double, so they are accurate when being multipled by 5/9 or 9/5.
+  */
+  public static double celsiusToFahrenheit(double Celsius1){
+    return 0;
+  }
+
+  public static double fahrenheitToCelsius(double Fahrenheit2){
+    return 1;
+  }
+
+  public static void main(String[] args){
+    System.out.println(celsiusToFahrenheit);
+    System.out.println(fahrenheitToCelsius);
+  }
+}
